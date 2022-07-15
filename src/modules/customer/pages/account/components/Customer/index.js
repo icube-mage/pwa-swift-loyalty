cee2904ae2cb0a428e444cbd70caad4121667cbb
staceySirclo <stacey.chu@sirclo.com>
2022-07-15 18:25:00 +0700

    fix(27332804) My Account : remove unused (notification, wishlist) call

--- src/modules/customer/pages/account/components/Customer/index.js
+++ src/modules/customer/pages/account/components/Customer/index.js
@@ -9,13 +9,7 @@ const Customer = (props) => {
         t, Skeleton, CustomerView, storeConfig, isLogin, reOrder, ...other
     } = props;
     let userData = {};
-    let wishlist = [];
     const { data, loading, error } = gqlService.getCustomer(storeConfig);
-    const { data: customerNotificationList } = gqlService.customerNotificationList();
-    const { data: customerOrders } = gqlService.getCustomerOrder();
-    const totalUnread = customerNotificationList
-        && customerNotificationList.customerNotificationList
-        && customerNotificationList.customerNotificationList.totalUnread;
 
     if (!data || loading || error) {
         return (
@@ -26,31 +20,11 @@ const Customer = (props) => {
     }
     if (data) {
         userData = data;
-        if (modules.wishlist.enabled) {
-            wishlist = data.customer
-                && data.customer.wishlist
-                && data.customer.wishlist.items.map(({ product }) => ({
-                    ...product,
-                    name: product.name,
-                    link: product.url_key,
-                    imageSrc: product.small_image.url,
-                    price: product.price_range.minimum_price.regular_price.value,
-                    showWishlistAction: false,
-                }));
-        }
     }
-
-    if (customerNotificationList && customerNotificationList.customerNotificationList) {
-        userData = { ...userData, notificationList: customerNotificationList.customerNotificationList };
-    }
-    if (customerOrders && customerOrders.customerOrders) {
-        userData = { ...userData, customerOrders: customerOrders.customerOrders };
-    }
-
     const menu = [
         { href: '/customer/account/profile', title: t('customer:menu:myAccount') },
         { href: '/sales/order/history', title: t('customer:menu:myOrder') },
-        { href: '/sales/transaction/history', title: 'Offline transaction' },
+        { href: '/swiftloyalty/offlinetransaction', title: 'Offline transaction' },
         { href: '/customer/account/address', title: t('customer:menu:address') },
         { href: '/customer/account/profile', title: t('customer:menu:accountInformation') },
     ];
@@ -63,8 +37,6 @@ const Customer = (props) => {
                 modules={modules}
                 menu={menu}
                 userData={userData}
-                totalUnread={totalUnread}
-                wishlist={wishlist}
                 storeConfig={storeConfig}
                 isLogin={isLogin}
                 reOrder={reOrder}
